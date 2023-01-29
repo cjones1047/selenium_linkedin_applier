6cb84035e807950b2820001c93f9a6c2b5ef0e42
cjones1047 <cjmj1047@gmail.com>
2023-01-29 14:27:26 -0600

    Automatically applies to each Easy Apply application that requires only one click; Notification provides via print() with each submission; Automatically skips over jobs previously applied for using exception handling of NoSuchElementException; Automatically moves to next page of listings when current page is completely browsed

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 import time
+from selenium.common.exceptions import NoSuchElementException
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-# from selenium.webdriver import ActionChains
 import os
 import dotenv
 from selenium.webdriver.common.keys import Keys
-# from datetime import datetime
-# import re
 
 
 class EasyApply:
@@ -18,9 +16,20 @@ class EasyApply:
         self.linkedin_password = os.getenv("LINKEDIN_PASSWORD")
         chrome_options = Options()
         chrome_options.add_experimental_option("detach", True)
+        # chrome_options.add_argument('--no-sandbox')
+        # chrome_options.add_argument('--start-maximized')
+        # chrome_options.add_argument('--start-fullscreen')
+        # chrome_options.add_argument('--single-process')
+        # chrome_options.add_argument('--disable-dev-shm-usage')
+        # chrome_options.add_argument("--incognito")
+        # chrome_options.add_argument('--disable-blink-features=AutomationControlled')
+        # chrome_options.add_argument('--disable-blink-features=AutomationControlled')
+        # chrome_options.add_experimental_option('useAutomationExtension', False)
+        # chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+        # chrome_options.add_argument("disable-infobars")
         self.driver = webdriver.Chrome(options=chrome_options)
-        flask_job_postings_url = ("https://www.linkedin.com/jobs/search/?f_AL=true&f_T=25169&"
-                                  "f_TPR=r604800&f_WT=2&geoId=103644278&keywords=flask")
+        flask_job_postings_url = ("https://www.linkedin.com/jobs/search/?distance=25&f_AL=true&"
+                                  "f_T=25169&f_WT=2&geoId=103644278&keywords=flask")
         self.driver.get(flask_job_postings_url)
         sign_in = self.driver.find_element(by=By.CSS_SELECTOR, value=('a[data-tracking-control-name='
                                                                       '"public_jobs_conversion-modal-signin"]'))
@@ -29,7 +38,8 @@ class EasyApply:
         self.driver.get(sign_in_page_link)
         username_box = self.driver.find_element(by=By.ID, value="username")
         username_box.send_keys(self.linkedin_username, Keys.TAB, self.linkedin_password, Keys.ENTER)
-        time.sleep(5)
+        input("Click here and press 'ENTER' after passing bot detection...")
+        self.current_page = 1
         self.build_job_posting_list()
 
     def build_job_posting_list(self):
@@ -38,9 +48,66 @@ class EasyApply:
         for _ in range(10):
             first_job_title_el.send_keys(Keys.PAGE_DOWN)
         job_title_els = self.driver.find_elements(by=By.CSS_SELECTOR, value=f"a[class='{job_title_link_class}']")
-        python_developer_job_title_els = [job for job in job_title_els if "Python Developer" in job.text]
+        python_developer_job_title_els = [job for job in job_title_els
+                                          if "Python" in job.text
+                                          and "remote" not in job.text.lower()]
         for job in python_developer_job_title_els:
             print(job.text)
+            self.apply_to_job(job)
+        else:
+            self.current_page += 1
+            next_page_button = self.driver.find_element(by=By.CSS_SELECTOR,
+                                                        value=f'button[aria-label="Page {self.current_page}"]')
+            next_page_button.click()
+            time.sleep(2)
+            self.build_job_posting_list()
+
+    def apply_to_job(self, job):
+        job.click()
+        time.sleep(1)
+
+        # skips jobs you have already applied to
+        try:
+            easy_apply_button = self.driver.find_element(by=By.CSS_SELECTOR,
+                                                         value='div[class="jobs-apply-button--top-card"]')
+        except NoSuchElementException:
+            return
+        easy_apply_button.click()
+        time.sleep(1)
+
+        # do not follow company by default:
+        try:
+            follow_company_checkbox = self.driver.find_element(by=By.CSS_SELECTOR,
+                                                               value='label[for="follow-company-checkbox"]')
+            follow_company_checkbox.click()
+            time.sleep(1)
+        except NoSuchElementException:
+            pass
+
+        try:
+            submit_application_button = self.driver.find_element(by=By.CSS_SELECTOR,
+                                                                 value='button[aria-label="Submit application"]')
+            submit_application_button.click()
+            time.sleep(3)
+            self.close_application()
+        except NoSuchElementException:
+            self.close_application()
+
+    def close_application(self):
+        dismiss_button = self.driver.find_element(by=By.CSS_SELECTOR,
+                                                  value='button[aria-label="Dismiss"]')
+        dismiss_button.click()
+        time.sleep(1)
+        try:
+            confirm_button = self.driver.find_element(by=By.CSS_SELECTOR,
+                                                      value=('button[data-control-name='
+                                                             '"discard_application_confirm_btn"]'))
+            confirm_button.click()
+        except NoSuchElementException:
+            print("Application submitted.")
+            print()
+        finally:
+            time.sleep(1)
 
 
 application = EasyApply()
